b5e6fe80ce0dc373284ae57f2f6396a7b5f8e884
ah744 <ah744@cornell.edu>
2017-03-10 21:14:11 -0600

    adding a grabber

---
+++ Algorithms/March10/grab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+loc="aholmes@everest.cs.uchicago.edu:/home/aholmes/Scaffold_Master_October4/scripts/"
+names=('binary_welded_tree.n100s100' 'boolean_formula.x2y2' 'ground_state_estimation.m10' 'ising_model.n100' 'qft.n05' 'sha1.n1024' 'shors.n4' 'shors.n512' 'square_root.n10')
+items=(10 100 500 1000 10000 100000 1000000)
+for key in "${names[@]}"
+do
+	mkdir -p $key
+	cd $key
+	scp $loc'full-parallel-'$key'/*lpfs' .
+	cd ../
+done
